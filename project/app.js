d3a39debff63a03a1f7c263c3dd7cec6ee1d6ba2
wenqiang <742241699@qq.com>
2018-05-21 17:15:09 +0800

    修改app.js  添加ejs

--- project/app.js
+++ project/app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var ejs=require('ejs');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html',ejs.__express);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
