d3ea3a6cf97a40ef7bf5a2c7ab80aaa7dc03bac4
feihaixia <943765494@qq.com>
2018-05-26 11:57:20 +0800

    aaa

--- project/routes/shop.js
+++ project/routes/shop.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',//主机名
-//   user     : 'root',//用户名
-//   password : '123456',//密码
-//   database : 'test'
-// });
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'qdm19023448.my3w.com',//主机名
+  user     : 'qdm19023448',//用户名
+  password : 'zeiyeaiq',//密码
+  database : 'qdm19023448_db',
+  insecureAuth:true
+});
 
-//  // 连接mysql数据库
-// connection.connect();
-// console.log('数据库连接成功');
+ // 连接mysql数据库
+connection.connect();
+console.log('数据库连接成功');
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
@@ -19,7 +20,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 	//将获取到的信息添加到数据库
-	var  Sql = 'SELECT * FROM t1';
+	var  Sql = 'SELECT * FROM go_shop';
 	console.log(Sql);
 
 	// 执行添加语句   connection(sql,data,callback)
