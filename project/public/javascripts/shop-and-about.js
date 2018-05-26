be1007f7310c257019b3744af3e6263ab72bc8d6
wangxi <1196990124@qq.com>
2018-05-26 14:05:51 +0800

    updata

--- project/public/javascripts/shop-and-about.js
+++ project/public/javascripts/shop-and-about.js
@@ -25,10 +25,10 @@ $(function(){
 				success:function(data){
 					var img=$('.afterColor img');
 					var p1=$('.p1');
-					console.log(p1);
 					var p2=$('.p2');
 					for (var i = 0; i<data.length; i++) {
-						img[i].src='images/about_img/'+data[i].pic+'.jpg'
+						// img[i].src='images/about_img/'+data[i].pic+'.jpg'
+						$(img[i]).attr('src','images/about_img/'+data[i].pic+'.jpg')
 						p1.text(data[i].description);
 						p2.text(data[i].much);
 					}
@@ -52,6 +52,47 @@ $(function(){
 				}
 				});
 			}
-		})
+		});
+	var prev=$('#prev');
+	 var next=$('#next');
+	 var aLi=$('#peopleDes .li')
+	 var index=2;
+	 function animate(offset) {
+		var peopleDes=$('#peopleDes');
+                var newLeft = parseInt(peopleDes.css('left')) + offset;
+                peopleDes.stop(true).animate({'left':newLeft + 'px'},'slow')
+            }
+            function remove(){
+            	     for (var i = 0; i <aLi.length; i++) {
+            		$(aLi[i]).removeClass('center');
+            		$(aLi[i]).on('click',function(){
+            			this.addClass('center');
+            		})
+            	}
+            }
+
+            prev.on('click',function(){
+            		animate(-198);
+            		remove();
+            		index+=1;
+            		console.log(index);
+            		$(aLi[index]).addClass('center').append('<h3>John Francis Doe</h3><h4>BARTENDER</h4><ul><li><i class=" icon-facebook"></i></li><li><a href="#"><i class=" icon-twitter"></i></a></li><li><a href="#"><i class="icon-google-plus"></i></a></li></ul>');
+            		$(aLi[index]).siblings().find('h3').remove();
+            		$(aLi[index]).siblings().find('h4').remove();
+            		$(aLi[index]).siblings().find('ul').remove();
+ 			          
+
+            });
+            
+            next.on('click',function(){
+            		remove();
+            		index-=1;
+            		$(aLi[index]).siblings().find('h3').remove();
+            		$(aLi[index]).siblings().find('h4').remove();
+            		$(aLi[index]).siblings().find('ul').remove();
+            		$(aLi[index]).addClass('center').append('<h3>John Francis Doe</h3><h4>BARTENDER</h4><ul><li><i class=" icon-facebook"></i></li><li><a href="#"><i class=" icon-twitter"></i></a></li><li><a href="#"><i class="icon-google-plus"></i></a></li></ul>');
+
+           	 }); 
+ 
 });
 
