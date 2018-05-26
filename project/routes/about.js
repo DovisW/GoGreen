bd84d22e390c94870fe3f7acbc1699fb99ee8730
wangxi <1196990124@qq.com>
2018-05-26 14:35:26 +0800

    删除 less

--- project/routes/about.js
+++ project/routes/about.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'qdm19023448.my3w.com',//主机名
+  user     : 'qdm19023448',//用户名
+  password : 'zeiyeaiq',//密码
+  database : 'qdm19023448_db',
+  insecureAuth:true
+});
+connection.connect();
+console.log('数据库连接成功');
 router.get('/', function(req, res, next) {
-  res.render('about');
+  res.render('about', { title: 'Express' });
+});
+router.post('/', function(req, res, next) {
+	var  Sql = 'SELECT * FROM go_list limit 6';
+	connection.query(Sql,function (err, result) {
+	        if(err){
+	         // console.log('[SELECT ERROR] - ',err.message);
+	         return;
+	        }   
+		res.send(result);   
+	});
 });
 
 module.exports = router;
+
